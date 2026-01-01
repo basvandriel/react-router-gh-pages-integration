1efc01c3739d5ada016c5a69998ad9ac94a719e8
Bas <contact@basvandriel.nl>
2026-01-01 19:55:43 +0100

    work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <div className="text-sm">Bas van Driel</div>,
+    },
+  ],
   {
-    path: "/",
-    element: <div className="text-sm">Bas van Driel</div>,
-  },
-]);
+    basename: "/react-router-gh-pages-integration",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
